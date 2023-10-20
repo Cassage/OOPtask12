44f951faff71a9025215b8a5cf25b033b65a82db
Cassage <mavrin.2003@list.ru>
2023-10-20 12:13:54 +0300

    done

---
+++ src/Composite/Circle.java
@@ -0,0 +1,11 @@
+package Composite;
+
+public class Circle implements Figure{
+    int height;
+    int width;
+
+    public Circle(int height, int width){
+        this.height = height;
+        this.width = width;
+    }
+}
